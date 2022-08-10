6d277c12fef2f1d0361239993bbf208f00c7e34d
Julian McGinnis <julian.mcginnis@tum.de>
2022-08-10 15:06:33 +0200

    add wrapper script for fsl-based data orientation

---
+++ source/utility/reorient.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Reorient sagittally scanned MRI images to axial orientation, complying to
+#
+# the stitching convention of the stitching algorithm by Ben Glocker.
+#
+# Usage:
+#   ./reorient.sh <SCAN>
+#
+# Authors: Julian McGinnis
+
+# Retrieve input params
+SCAN=$1
+
+# assemble name
+SCAN_NAME=${SCAN%%.*}
+FILE_ENDING="_axial_ornt.nii.gz"
+FILE_PATH="$SCAN_NAME$FILE_ENDING"
+
+echo "Creating $FILE_PATH"
+
+# get starting time:
+start=`date +%s`
+
+echo "Swaping Dimensions"
+fslswapdim ${SCAN} LR AP IS ${FILE_PATH}
+echo "Forcing neuological orientation"
+fslorient -forceneurological ${FILE_PATH}
+
+# Display useful info for the log
+end=`date +%s`
+runtime=$((end-start))
+echo "Duration: $(($runtime / 3600))hrs $((($runtime / 60) % 60))min $(($runtime % 60))sec"
