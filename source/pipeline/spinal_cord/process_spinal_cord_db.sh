87791e92c8d7e4d90348973e88293bf937db64f8
Julian McGinnis <julian.mcginnis@tum.de>
2022-09-27 13:34:26 +0200

    add first draft of the spinal cord processing pipeline

---
+++ source/pipeline/spinal_cord/process_spinal_cord_db.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+#
+# Process data. This script is designed to be run in the folder for a single subject, however 'sct_run_batch' can be
+# used to run this script multiple times in parallel across a multi-subject BIDS dataset.
+#
+# Usage:
+#   ./process_data.sh <SUBJECT>
+#
+# Example:
+#   ./process_data.sh sub-03
+#
+# Author: Julien Cohen-Adad, Julian McGinnis
+
+# The following global variables are retrieved from the caller sct_run_batch
+# but could be overwritten by uncommenting the lines below:
+# PATH_DATA_PROCESSED="~/data_processed"
+# PATH_RESULTS="~/results"
+# PATH_LOG="~/log"
+# PATH_QC="~/qc"
+
+
+# BASH SETTINGS
+# ======================================================================================================================
+
+# Uncomment for full verbose
+set -x
+
+# Immediately exit if error
+set -e
+
+# Exit if user presses CTRL+C (Linux) or CMD+C (OSX)
+trap "echo Caught Keyboard Interrupt within script. Exiting now.; exit" INT
+
+
+# CONVENIENCE FUNCTIONS
+# ======================================================================================================================
+
+# Check if manual label already exists. If it does, copy it locally. If it does
+# not, perform labeling.
+label_if_does_not_exist(){
+  local file="$1"
+  local file_seg="$2"
+  # Update global variable with segmentation file name
+  FILELABEL="${file}_labels-disc"
+  FILELABELMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECTSESSION}/anat/${FILELABEL}-manual.nii.gz"
+  echo "Looking for manual label: $FILELABELMANUAL"
+  if [[ -e $FILELABELMANUAL ]]; then
+    echo "Found! Using manual labels."
+    rsync -avzh $FILELABELMANUAL ${FILELABEL}.nii.gz
+    # Generate labeled segmentation from manual disc labels
+    sct_label_vertebrae -i ${file}.nii.gz -s ${file_seg}.nii.gz -discfile ${FILELABEL}.nii.gz -c t1 -qc "${PATH_QC}" -qc-subject "${SUBJECTSESSION}"
+  else
+    echo "Not found. Proceeding with automatic labeling."
+    # Generate labeled segmentation
+    sct_label_vertebrae -i ${file}.nii.gz -s ${file_seg}.nii.gz -c t1 -qc "${PATH_QC}" -qc-subject "${SUBJECTSESSION}"
+  fi
+}
+
+segment_if_does_not_exist() {
+  ###
+  #  This function checks if a manual spinal cord segmentation file already exists, then:
+  #    - If it does, copy it locally.
+  #    - If it doesn't, perform automatic spinal cord segmentation.
+  #  This allows you to add manual segmentations on a subject-by-subject basis without disrupting the pipeline.
+  ###
+  local file="$1"
+  local contrast="$2"
+  # Update global variable with segmentation file name
+  FILESEG="${file}_seg"
+  FILESEGMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECTSESSION}/anat/${FILESEG}-manual.nii.gz"
+  echo
+  echo "Looking for manual segmentation: $FILESEGMANUAL"
+  if [[ -e $FILESEGMANUAL ]]; then
+    echo "Found! Using manual segmentation."
+    rsync -avzh $FILESEGMANUAL ${FILESEG}.nii.gz
+    sct_qc -i ${file}.nii.gz -s ${FILESEG}.nii.gz -p sct_deepseg_sc -qc ${PATH_QC} -qc-subject ${SUBJECTSESSION}
+  else
+    echo "Not found. Proceeding with automatic segmentation."
+    # Segment spinal cord
+    sct_deepseg_sc -i ${file}.nii.gz -c $contrast -qc ${PATH_QC} -qc-subject ${SUBJECTSESSION}
+  fi
+}
+
+# SCRIPT STARTS HERE
+# ======================================================================================================================
+
+# Retrieve input params
+SUBJECTSESSION=$1
+echo $SUBJECTSESSION
+
+# get starting time:
+start=`date +%s`
+
+# Display useful info for the log, such as SCT version, RAM and CPU cores available
+sct_check_dependencies -short
+
+# Go to folder where data will be copied and processed
+cd $PATH_DATA_PROCESSED
+
+# Copy source images
+# We use '/./' in order to include the sub-folder 'ses-0X'
+rsync -Ravzh $PATH_DATA/./$SUBJECTSESSION .
+
+# Go to subject folder for source images
+cd ${SUBJECTSESSION}/anat
+
+# Define variables
+# We do a substitution '/' --> '_' in case there is a subfolder 'ses-0X/'
+file="${SUBJECTSESSION//[\/]/_}"
+
+
+# T2w
+# ======================================================================================================================
+file_t2w="${file}_T2w"
+echo $file_t2w
+
+file_t2w_sag="${file}_acq-sag_T2w"
+file_t2w_ax="${file}_acq-ax_T2w"
+
+# Make sure q/sform are the same
+sct_image -i ${file_t2w_sag}.nii.gz -set-sform-to-qform
+sct_image -i ${file_t2w_ax}.nii.gz -set-sform-to-qform
+
+# Segment spinal cord (only if it does not exist)
+segment_if_does_not_exist "${file_t2w_sag}" "t2"
+file_t2w_sag_seg="${FILESEG}"
+
+# Create labels in the cord 
+label_if_does_not_exist "${file_t2w_sag}" "${file_t2w_sag_seg}"
+file_t2w_sag_seg_labeled="${file_t2w_sag_seg}_labeled"
+
+# Compute average CSA between C1 and C7 levels 
+sct_process_segmentation -i "${file_t2w_sag_seg}.nii.gz" -vert 1:7 -vertfile ${file_t2w_sag_seg_labeled}.nii.gz \
+                         -o "${PATH_RESULTS}/csa_perlevel.csv" -perlevel 1 -append 1 -qc "${PATH_QC}"
+
+
+FILES_TO_CHECK=(
+  "$file_t2w_sag_seg.nii.gz"
+)
+for file in "${FILES_TO_CHECK[@]}"; do
+  if [ ! -e "${file}" ]; then
+    echo "${SUBJECTSESSION}/${file} does not exist" >> "${PATH_LOG}/error.log"
+  fi
+done
+
+# Display useful info for the log
+end=`date +%s`
+runtime=$((end-start))
+echo
+echo "~~~"
+echo "SCT version: `sct_version`"
+echo "Ran on:      `uname -nsr`"
+echo "Duration:    $(($runtime / 3600))hrs $((($runtime / 60) % 60))min $(($runtime % 60))sec"
+echo "~~~"
